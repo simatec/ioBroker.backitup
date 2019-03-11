5766359c80de3023782950145b07ce70cf45bb17
Ingo Fischer <github@fischer-ka.de>
2019-03-11 22:22:50 +0100

    fix unit testing

--- test/unit.js
+++ test/unit.js
@@ -1,6 +1,11 @@
 const path = require('path');
 const { tests } = require('@iobroker/testing');
 
+const jscToolsMock = {
+	// No update checks in unit tests
+	getInstalledInfo() { return {}; },
+};
+
 // Run unit tests - See https://github.com/ioBroker/testing for a detailed explanation and further options
 tests.unit(path.join(__dirname, '..'), {
 	additionalMockedModules: {
