d630e0ecefb124228317d24821aeb98e68e1bc02
Andrey Kuznetsoff <dfrozen@ya.ru>
2020-03-26 14:19:05 +0300

    revert

--- main.js
+++ main.js
@@ -395,6 +395,7 @@ function initConfig(secret) {
         port: adapter.config.mySqlPort,                // database port
         exe: adapter.config.mySqlDumpExe               // path to mysqldump
     };
+<<<<<<< HEAD
 // Postgress SQL
 	 const pgsql = {
         enabled: adapter.config.mySqlEnabled === undefined ? true : adapter.config.mySqlEnabled,
@@ -416,6 +417,9 @@ function initConfig(secret) {
     };
 // Postgress SQL
 	
+=======
+
+>>>>>>> parent of bdd5c16... Develop changes PGSQL
     // Configurations for standard-IoBroker backup
     backupConfig.iobroker = {
         name: 'iobroker',
@@ -430,8 +434,12 @@ function initConfig(secret) {
         cifs: Object.assign({}, cifs, (adapter.config.cifsOwnDir === true) ? {dir:  adapter.config.cifsMinimalDir}  : {}),
         dropbox: Object.assign({}, dropbox, (adapter.config.dropboxOwnDir === true) ? {dir:  adapter.config.dropboxMinimalDir}  : {}),
         googledrive: Object.assign({}, googledrive, (adapter.config.googledriveOwnDir === true) ? {dir:  adapter.config.googledriveMinimalDir}  : {}),
+<<<<<<< HEAD
       
 	    mysql: {
+=======
+        mysql: {
+>>>>>>> parent of bdd5c16... Develop changes PGSQL
             enabled: adapter.config.mySqlEnabled === undefined ? true : adapter.config.mySqlEnabled,
             type: 'creator',
             ftp:  Object.assign({}, ftp,  (adapter.config.ftpOwnDir === true) ? {dir:  adapter.config.ftpMinimalDir} : {}),
@@ -449,6 +457,7 @@ function initConfig(secret) {
             port: adapter.config.mySqlPort,                // database port
             exe: adapter.config.mySqlDumpExe               // path to mysqldump
         },
+<<<<<<< HEAD
 	//PgSQL
 	    	    pgsql: {
             enabled: adapter.config.mySqlEnabled === undefined ? true : adapter.config.mySqlEnabled,
@@ -470,6 +479,8 @@ function initConfig(secret) {
         },
 	//PgSQL    
 	    
+=======
+>>>>>>> parent of bdd5c16... Develop changes PGSQL
         dir: tools.getIobDir(),
 		redis: {
 			enabled: adapter.config.redisEnabled,
