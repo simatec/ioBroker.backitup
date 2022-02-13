a9e2e67921bc069945859e8481d320e6a73df7a5
simatec <nais@gmx.net>
2022-02-13 17:35:59 +0100

    (simatec) Fix Docker restore

--- main.js
+++ main.js
@@ -891,7 +891,7 @@ function createBashScripts() {
         }
 
         try {
-            fs.writeFileSync(bashDir + '/startIOB.sh', `#!/bin/bash\n# iobroker start after restore\nif [ -f ${bashDir}/.startAll ]; then\ncd "${path.join(tools.getIobDir())}"\niobroker start all;\nfi\ngosu root /opt/scripts/maintenance.sh off -y`);
+            fs.writeFileSync(bashDir + '/startIOB.sh', `#!/bin/bash\n# iobroker start after restore\nif [ -f ${bashDir}/.startAll ]; then\ncd "${path.join(tools.getIobDir())}"\niobroker start all;\nfi\nsleep 5\ngosu root /opt/scripts/maintenance.sh off -y`);
             fs.chmodSync(bashDir + '/startIOB.sh', 508);
         } catch (e) {
             adapter.log.error('cannot create startIOB.sh: ' + e + 'Please run "iobroker fix"');
