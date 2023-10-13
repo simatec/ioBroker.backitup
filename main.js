ef8baa7291b8f6cd789679d3aa0e3f52bda2072f
simatec <nais@gmx.net>
2023-10-13 21:09:03 +0200

    (simatec) Fix mount

--- main.js
+++ main.js
@@ -1672,10 +1672,11 @@ async function main(adapter) {
     readLogFile();
 
     // only for node 20 bug testing
+    /*
     adapter.log.debug('UV_USE_IO_URING: ' + process.env.UV_USE_IO_URING);
     process.env['UV_USE_IO_URING'] = 0;
     adapter.log.debug('UV_USE_IO_URING: ' + process.env.UV_USE_IO_URING);
-
+    */
     if (!fs.existsSync(path.join(tools.getIobDir(), 'backups'))) createBackupDir();
     if (fs.existsSync(bashDir + '/.redis.info')) deleteHideFiles();
     if (fs.existsSync(path.join(tools.getIobDir(), 'backups/tmp'))) delTmp();
