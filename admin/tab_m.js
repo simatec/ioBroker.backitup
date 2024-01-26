a607bd693a229c346e194664ac68fcad9d23f050
simatec <nais@gmx.net>
2024-01-26 23:39:04 +0100

    (simatec) Customised design

--- admin/tab_m.js
+++ admin/tab_m.js
@@ -280,9 +280,9 @@ function load(settings, onChange) {
                                     text += '<ul class="collapsible"><li><div class="collapsible-header"><i class="material-icons">expand_more</i><h6>' + storage.toUpperCase() + '</h6></div>';
                                     text += '<ul class="collapsible-body collection">';
                                     for (var i = data[type][storage].length - 1; i >= 0; i--) {
-                                        text += '<li class="collection-item"><div>' + getName(data[type][storage][i].name) + ' <b>>>> ' + data[type][storage][i].name + ' <<<</b> (' + getSize(data[type][storage][i].size) + ')' +
-                                            '<a class="secondary-content do-restore" title="' + _('Restore Backup File') + '" data-file="' + data[type][storage][i].path + '" data-type="' + type + '"><i class="material-icons">restore</i></a>' +
-                                            '<a class="secondary-content do-download" title="' + _('Download Backup File') + '" data-file="' + data[type][storage][i].path + '" data-type="' + type + '"><i class="material-icons">file_download</i></a>' +
+                                        text += '<li title="' + data[type][storage][i].name + '" class="collection-item"><div>' + _('backup time') + ': ' + getName(data[type][storage][i].name) + ' | ' + _('filesize') + ': ' + getSize(data[type][storage][i].size) +
+                                            '<a class="btn-floating secondary-content do-restore" style="height: 32.4px!important; width: 32.4px!important;" title="' + _('Restore Backup File') + '" data-file="' + data[type][storage][i].path + '" data-type="' + type + '"><i style="line-height: 32.4px;" class="material-icons">settings_backup_restore</i></a>' +
+                                            '<a class="btn-floating secondary-content do-download" style="height: 32.4px!important; width: 32.4px!important;" title="' + _('Download Backup File') + '" data-file="' + data[type][storage][i].path + '" data-type="' + type + '"><i style="line-height: 32.4px;" class="material-icons">file_download</i></a>' +
                                             '</div></li>';
                                     }
                                     text += '</ul></li></ul>';
